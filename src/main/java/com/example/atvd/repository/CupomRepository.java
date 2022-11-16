5ee5745f636dfbbd334d76b9217dbea026eecfa3
cvjso <cvjso@cesar.org.br>
2022-11-16 11:05:54 -0300

    removed unnecessary files

--- src/main/java/com/example/atvd/repository/CupomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.atvd.repository;
-
-import com.example.atvd.entities.Cupom;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CupomRepository extends CrudRepository<Cupom, Long> {
-}
--- src/main/java/com/example/atvd/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.atvd.repository;
-
-import com.example.atvd.entities.Produto;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends CrudRepository<Produto, Long> {
-}
